3291bdece10fa0e3b65ca047b064cbee6b0fdca9
Calvin -Win10 <ApexCalvin@gmail.com>
2024-03-06 20:34:06 -0500

    annotated Card entity

--- src/main/kotlin/com/watcher/Watcher/API/entity/Card.kt
+++ src/main/kotlin/com/watcher/Watcher/API/entity/Card.kt
@@ -1,10 +1,15 @@
 package com.watcher.Watcher.API.entity
 
+import jakarta.persistence.*
+
+@Entity
 data class Card(
-    var id: Long,
-    var name: String,
-    var rarity: Char,
-    var set: String,
-    var foil: Boolean,
-    var purchasePrice: Double
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+    var name: String = "",
+    var rarity: Char = 'C',
+    var set: String = "",
+    var foil: Boolean = false,
+    var purchasePrice: Double = 00.00
 )
